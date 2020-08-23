2ceb0f05efd0b85cf167f9cc5c4af936ec5b4af6
Luke Gofford <20703554+ETFairfax@users.noreply.github.com@users.noreply.github.com>
2020-08-23 19:51:42 +0100

    Clean up

--- src/features/Detail/index.tsx
+++ src/features/Detail/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
-// import { useSelector } from 'store/hooks/useSelector'
 
 const Detail: React.FC = () => {
     let { group } = useParams()
