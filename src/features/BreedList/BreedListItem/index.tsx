b312cbeb747ce95e4d50590cfa3a63f157925db0
Luke Gofford <20703554+ETFairfax@users.noreply.github.com@users.noreply.github.com>
2020-08-23 19:40:02 +0100

    Added Link

--- src/features/BreedList/BreedListItem/index.tsx
+++ src/features/BreedList/BreedListItem/index.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import { IBreedGroup } from 'store/breeds/types'
 
 const BreedListItem: React.FC<IBreedGroup> = ({ name, breeds }) => (
     <div>
-        {name} {breeds.length}
+        <Link to={`/${name}`}>
+            {name} {breeds.length}
+        </Link>
     </div>
 )
 
--- src/features/BreedList/index.tsx
+++ src/features/BreedList/index.tsx
@@ -1,10 +1,6 @@
 import React from 'react'
-// import TaskItem from 'features/TaskList/TaskItem'
 import { useSelector } from 'store/hooks/useSelector'
 import BreedListItem from './BreedListItem'
-// import { useDispatch } from 'react-redux'
-// import { deleteTaskThunk } from 'store/breeds/thunks'
-// import About from 'features/About'
 
 const BreedList: React.FC = () => {
     const { breedGroups } = useSelector(({ breedGroups }) => breedGroups)
